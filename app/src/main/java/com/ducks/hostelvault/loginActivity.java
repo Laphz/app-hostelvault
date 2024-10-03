a3418f0407137b51f8312b14e47126f0422cbd54
Laphz <aman2005mishra@gmail.com>
2024-10-04 00:03:08 +0530

    kuch ni badla

--- app/src/main/java/com/ducks/hostelvault/loginActivity.java
+++ app/src/main/java/com/ducks/hostelvault/loginActivity.java
@@ -1,5 +1,7 @@
 package com.ducks.hostelvault;
 
+import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -32,6 +34,15 @@ public class loginActivity extends AppCompatActivity {
 
     private FirebaseHelper firebaseHelper;
     private HelperClass helperClass;
+
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        Configuration overrideConfiguration = new Configuration(newBase.getResources().getConfiguration());
+        overrideConfiguration.fontScale = 1.0f;  // Set fontScale to 1.0 to avoid scaling
+        Context context = newBase.createConfigurationContext(overrideConfiguration);
+        super.attachBaseContext(context);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
