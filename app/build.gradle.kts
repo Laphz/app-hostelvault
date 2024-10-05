cb2fc73c17e8d82fb639e5530cda1b8ca9b6dbac
Laphz <aman2005mishra@gmail.com>
2024-10-05 12:37:30 +0530

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.ducks.hostelvault"
         minSdk = 27
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
